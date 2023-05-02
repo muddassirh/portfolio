50612d155489942b3a466e0ae3387a2bc1f8cb55
Muddassir <genxsmedia@gmail.com>
2023-05-02 22:10:48 +0500

    Create portfolio.js

---
+++ src/data/portfolio.js
@@ -0,0 +1,104 @@
+
+export const info = {
+    firstName: "Muddassir",
+    lastName: "Hassan",
+    position: "Frontend UX/UI Designer and Full Stack Developer",
+    baseColor: colors[0],
+    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+        {
+            text: 'become energetic with RC Cars'
+        },
+        
+        {
+            text: "free no - yes I am a freelancer"
+        },
+        {
+            text: "me@genxsmedia.com"
+        }
+    ],
+    socials: [
+        {
+            link: "https://facebook.com",
+            icon: 'fa fa-facebook',
+            label: 'facebook'
+        },
+        {
+            link: "https://instagram.com",
+            icon: 'fa fa-instagram',
+            label: 'instagram'
+        },
+        {
+            link: "https://github.com",
+            icon: "fa fa-github",
+            label: 'github'
+        },
+        {
+            link: "https://linkedin.com",
+            icon: "fa fa-linkedin",
+            label: 'linkedin'
+        },
+        {
+            link: "https://twitter.com",
+            icon: "fa fa-twitter",
+            label: 'twitter'
+        }
+
+    ],
+    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    skills:
+        {
+            proficientWith: ['html5', 'CSS3', 'git', 'github', 'bootstrap', 'react', 'figma', 'xd'],
+            exposedTo: ['adobe photoshop', 'adobe illustrator']
+        }
+    ,
+    hobbies: [
+        {
+            label: 'reading',
+            emoji: '📖'
+        },
+        {
+            label: 'theater',
+            emoji: '🎭'
+        },
+        {
+            label: 'movies',
+            emoji: '🎥'
+        },
+        {
+            label: 'cooking',
+            emoji: '🌶'
+        }
+    ],
+    portfolio: [ // This is where your portfolio projects will be detailed
+        {
+            title: "Project 1",
+            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
+            image: mock1
+        },
+        {
+            title: "Project 2",
+            live: "https://paytonpierce.dev",
+            source: "https://github.com/paytonjewell",
+            image: mock2
+        },
+        {
+            title: "Project 3",
+            live: "https://paytonpierce.dev",
+            source: "https://github.com/paytonjewell",
+            image: mock3
+        },
+        {
+            title: "Project 4",
+            live: "https://paytonpierce.dev",
+            source: "https://github.com/paytonjewell",
+            image: mock4
+        },
+        {
+            title: "Project 5",
+            live: "https://paytonpierce.dev",
+            source: "https://github.com/paytonjewell",
+            image: mock5
+        }
+    ]
+}
