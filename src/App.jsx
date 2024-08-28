6438c97581c7c5ba5e2659ce0ea5d42649d0d0b7
muddassirh <genxsmedia@gmail.com>
2024-08-28 07:41:24 +0500

    some-errors removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
