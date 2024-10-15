7c1d83e372c6c2ab006d965c77d19b520df4a23f
Mohamamd Tahir <153367147+mtbaloch@users.noreply.github.com>
2024-10-15 21:16:50 +0500

    revert changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -47,7 +47,6 @@ import brandImg03 from '../components/assets/img/brand/h3_brand_img03.png';
 import brandImg04 from '../components/assets/img/brand/h3_brand_img04.png';
 import brandImg05 from '../components/assets/img/brand/h3_brand_img05.png';
 import brandImg06 from '../components/assets/img/brand/h3_brand_img06.png';
-import Testimonial1 from '../components/Testimonial1';
 
 
 const swiperOptions = {
@@ -396,8 +395,79 @@ export default function AboutUs() {
                 </section>*/}
                 {/* history-area-end */}
             {/* testimonial-area */}
-          
-            <Testimonial1/>
+            <section className="testimonial-area-three testimonial-area-four">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-md-8">
+                            <div className="section-title title-style-two mb-45">
+                                <h2 className="title">What Our Client’s Say</h2>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="testimonial-nav">
+                                <button className="swiper-button-prev" />
+                                <button className="swiper-button-next" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="swiper-container testimonial-active-three">
+                                <Swiper {...swiperOptions}>
+                                    <SwiperSlide>
+                                        <div className="testimonial-item-three">
+                                            <div className="testimonial-thumb-three">
+                                                <img src={testimonialAvatar1} alt="" />
+                                            </div>
+                                            <div className="testimonial-content-three">
+                                                <h4 className="title">James Botosh</h4>
+                                                <span>Product Designer</span>
+                                                <p>“We seek to get involved early in the design phase so that we can manage the project more efficiently, provide effective building solutions”</p>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="testimonial-item-three">
+                                            <div className="testimonial-thumb-three">
+                                                <img src={testimonialAvatar2} alt="" />
+                                            </div>
+                                            <div className="testimonial-content-three">
+                                                <h4 className="title">James Botosh</h4>
+                                                <span>Product Designer</span>
+                                                <p>“We seek to get involved early in the design phase so that we can manage the project more efficiently, provide effective building solutions”</p>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="testimonial-item-three">
+                                            <div className="testimonial-thumb-three">
+                                                <img src={testimonialAvatar3} alt="" />
+                                            </div>
+                                            <div className="testimonial-content-three">
+                                                <h4 className="title">James Botosh</h4>
+                                                <span>Product Designer</span>
+                                                <p>“We seek to get involved early in the design phase so that we can manage the project more efficiently, provide effective building solutions”</p>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className="testimonial-item-three">
+                                            <div className="testimonial-thumb-three">
+                                                <img src={testimonialAvatar1} alt="" />
+                                            </div>
+                                            <div className="testimonial-content-three">
+                                                <h4 className="title">James Botosh</h4>
+                                                <span>Product Designer</span>
+                                                <p>“We seek to get involved early in the design phase so that we can manage the project more efficiently, provide effective building solutions”</p>
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                </Swiper>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
             {/* testimonial-area-end */}
             {/* brand-area */}
             <div className="brand-area px-24 pb-120">
